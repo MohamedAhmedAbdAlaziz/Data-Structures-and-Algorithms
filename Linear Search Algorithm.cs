986cc503e1d5471cdc0674c70b5e5c7ad3461387
MohamedAhmedAbdAlaziz <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2021-07-08 19:49:37 +0200

    Add files via upload

---
+++ Linear Search Algorithm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] A = { 4, 5, 6, 3, 7, 2, 3 };
+            Console.WriteLine( LinearSearch(A, 66));
+
+
+           
+        }
+
+        public static int LinearSearch(int [] A , int key )
+        {
+            int index=0;
+            while(index< A.Length)
+            {
+                if (key == A[index])
+                    return key;
+                index++;
+            }
+            return -1;
+        }
+    }
+}
